4fedf13cc16cf345a92a4bf7c79dc66389d6a25a
Fahad Qazi <fahad.qazi@nanoporetech.com>
2019-05-23 12:38:11 +0100

    Added new component using interpolation, *ngIf and *ngFor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductList } from './products/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, ProductList
   ],
   imports: [
     BrowserModule
